7d5d8d30c66ff030d3d8b4c6e5ce212d315d2915
asm2002 <asm2002@live.ca>
2016-08-11 17:52:59 -0400

    Update packAttack.js

--- packAttack.js
+++ packAttack.js
@@ -8,11 +8,13 @@ function preload() {
     
     game.load.spritesheet('ship', 'sprites/pacman_by_oz_28x28.png', 28, 28);
     
-    game.load.image('bullet', 'http://individual.icons-land.com/IconsPreview/3D-Food/PNG/16x16/Fruit_Cherry.png');
+    game.load.image('bullet', 'sprites/tomato.png');
     
-    game.load.image('monster', 'http://cdn.iconsmash.com/Content/icons/Paradise_Fruits_Icon_Set_by_artbees/iconpreviews/16/Cherry.png');
+//    game.load.image('monster', 'http://cdn.iconsmash.com/Content/icons/Paradise_Fruits_Icon_Set_by_artbees/iconpreviews/16/Cherry.png');
 
 }
+//http://images.nationalgeographic.com/wpf/media-live/photos/000/012/cache/stars_1230_600x450.jpg
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -22,7 +24,7 @@ var starfield;
 var ship;
 var bullets;
 var nextFire = 0;
-var fireRate = 50;
+var fireRate = 100;
 
 var monsters;
 var numberOfMonsters = 10;
@@ -61,7 +63,7 @@ monsters = game.add.group();
 monsters.enableBody = true;
 monsters.phisicsBodyType = Phaser.Physics.ARCADE;
 
-createMonsters();
+/*createMonsters();
 
 function createMonsters(){
     
@@ -81,84 +83,64 @@ function createMonsters(){
     }
     
 }
-
+*/
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//Paste In 'create' 
-var starfield;
-var ship;
-var bullets;
-var nextFire = 0;
-var fireRate = 50;
-
-var monsters;
-var numberOfMonsters = 10;
-var speedOfMonsters = 50;
-
-var tolerance = 500;
-
-function create() {
+//Paste In 'update'
+function update() {
+ 
+     movementControls();
+   
+    shipFireControls();
     
-game.world.setBounds(0, 0, 800, 600);
-game.physics.startSystem(Phaser.Physics.P2JS);
-game.physics.p2.defaultRestitution = 0.9;
+    function fire() {
+    if (game.time.now > nextFire && bullets.countDead() > 0){
+        nextFire = game.time.now + fireRate;
+        var bullet = bullets.getFirstDead();
+        bullet.reset(ship.x + 25, ship.y + 25);
+        game.physics.arcade.moveToPointer(bullet, 600);
+    }
+}
 
-starfield = game.add.tileSprite(0, 0, 800, 600, "stars");
-starfield.fixedToCamera = true;
-
-ship = game.add.sprite(400, 300, 'ship');
-ship.scale.set(2);
-ship.smoothed = false;
-ship.animations.add('fly', [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 9, 9, 8, 8, 7, 7, 6, 6, 5, 5, 4, 4], 20, true);
-ship.play('fly');
-
-game.physics.enable(ship, Phaser.Physics.ARCADE);
-game.camera.follow(ship);
-ship.body.collideWorldBounds = true;
-
-bullets = game.add.group();
-bullets.enableBody = true;
-bullets.physicsBodyType = Phaser.Physics.ARCADE;
-
-bullets.createMultiple(50, 'bullet');
-bullets.setAll('checkWorldBounds', true);
-bullets.setAll('outOfBoundsKill', true);
-
-monsters = game.add.group();
-monsters.enableBody = true;
-monsters.phisicsBodyType = Phaser.Physics.ARCADE;
-
-createMonsters();
-
-function createMonsters(){
+function movementControls(){
     
-    var count = 0;
-    var inverseToTolerance = tolerance;
+    wasdControls = {
+        up: game.input.keyboard.addKey(Phaser.Keyboard.W),
+        down: game.input.keyboard.addKey(Phaser.Keyboard.S),
+        left: game.input.keyboard.addKey(Phaser.Keyboard.A),
+        right: game.input.keyboard.addKey(Phaser.Keyboard.D)
+    };
     
-    while(count < numberOfMonsters){
-        inverseToTolerence *= -1;
-        var randx = game.world.randomX + inverseToTolerance;
-        var randy = game.world.randomY + inverseToTolerance;
-        
-        if(ship.position.x - tolerance < randx === randx < ship.position.x + tolerance){
-            continue;
-        }
-        count++;
-        var monster = monsters.create(randx, randy, 'monster');
+    ship.body.velocity.setTo(0, 0);
+
+    if (wasdControls.left.isDown){
+		ship.body.velocity.x = -400;
+		
+    }
+    else if (wasdControls.right.isDown){
+		ship.body.velocity.x = 400;
+    }
+
+    if (wasdControls.up.isDown){
+    	ship.body.velocity.y = -400;
+    }
+    else if (wasdControls.down.isDown){
+        ship.body.velocity.y = 400;
     }
     
 }
 
+function shipFireControls(){
+    game.physics.arcade.angleToPointer(ship);
+    
+    if (game.input.activePointer.isDown){
+        fire();
+    }
 
-
-
-
-
-
-
+}
 
 }
