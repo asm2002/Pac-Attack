46437c471a8396f5a047be4903c222796aef621b
asm2002 <asm2002@live.ca>
2016-08-12 11:56:02 -0400

    Update packAttack.js

--- packAttack.js
+++ packAttack.js
@@ -5,75 +5,131 @@ function preload() {
     game.load.crossOrigin = 'anonymous';
         
     game.load.image('stars', 'http://images.nationalgeographic.com/wpf/media-live/photos/000/012/cache/stars_1230_600x450.jpg');
+    game.load.image('hud', 'textures/metal.png');
     
     game.load.spritesheet('ship', 'sprites/pacman_by_oz_28x28.png', 28, 28);
     
     game.load.image('bullet', 'sprites/tomato.png');
     
-//    game.load.image('monster', 'http://cdn.iconsmash.com/Content/icons/Paradise_Fruits_Icon_Set_by_artbees/iconpreviews/16/Cherry.png');
+    game.load.image('monster', 'https://lh3.googleusercontent.com/-RDc-rrc1Ta8/AAAAAAAAAAI/AAAAAAAAAJc/LF8uBEcQkLU/s46-c-k-no/photo.jpg');
+    
+    game.load.spritesheet('heart', 'http://megaicons.net/static/img/icons_sizes/333/864/32/pac-man-icon.png', 28, 28);
 
 }
-//http://images.nationalgeographic.com/wpf/media-live/photos/000/012/cache/stars_1230_600x450.jpg
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //Paste In 'create' 
+
+
+//variables
 var starfield;
+
+var hud;
+var hud2;
+
 var ship;
+
 var bullets;
 var nextFire = 0;
 var fireRate = 100;
 
 var monsters;
-var numberOfMonsters = 10;
-var speedOfMonsters = 50;
+var numberOfMonsters = 100;
+var monsterSpeed = 50;
 
-var tolerance = 500;
+var tolerance = 200;
+
+var hearts;
+
+var numberOfLives = 5;
 
 function create() {
     
-game.world.setBounds(0, 0, 800, 600);
-game.physics.startSystem(Phaser.Physics.P2JS);
-game.physics.p2.defaultRestitution = 0.9;
+    //make world
+    game.world.setBounds(0, 0, 800, 540);
+    game.physics.startSystem(Phaser.Physics.P2JS);
+    game.physics.p2.defaultRestitution = 0.9;
+    
+    //set background
+    starfield = game.add.tileSprite(0, 0, 800, 600, "stars");
+    starfield.fixedToCamera = true;
+    
+    //make ship
+    ship = game.add.sprite(400, 300, 'ship');
+    ship.scale.set(2);
+    ship.smoothed = false;
+    ship.animations.add('fly', [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4], 10, true);
+    ship.play('fly');
+    
+    
+    //ship atributes
+    game.physics.enable(ship, Phaser.Physics.ARCADE);
+    game.camera.follow(ship);
+    ship.body.collideWorldBounds = true;
+    
+    //make bullets
+    bullets = game.add.group();
+    bullets.enableBody = true;
+    bullets.physicsBodyType = Phaser.Physics.ARCADE;
+    
+    
+    //bullet atributes
+    bullets.createMultiple(50, 'bullet');
+    bullets.setAll('checkWorldBounds', true);
+    bullets.setAll('outOfBoundsKill', true);
+    
+    //make monsters
+    monsters = game.add.group();
+    monsters.enableBody = true;
+    monsters.physicsBodyType = Phaser.Physics.ARCADE;
+    
+    createMonsters();
+    
+    //HUD
+    hud = game.add.sprite(0, 540, 'hud');
+    hud2 = game.add.sprite(512, 540, 'hud')
+    
+    //make hearts
+    hearts = game.add.group();
+    hearts.fixedToCamera = true;
+    lifeText = game.add.text(20, 560, 'Lives: ', {
+        font: '32px 8-bit',
+        fill: '#fff'
+    });
+    lifeText.fixedToCamera = true;
+    createLives();
+    
+    //gameover u noob
+   gameOverText = game.add.text()
 
-starfield = game.add.tileSprite(0, 0, 800, 600, "stars");
-starfield.fixedToCamera = true;
+function start(){
+    
+    game.time.reset();
+    
+    hearts.callAll('revive');
+    monsters.removeAll;
+    
+    createMonsters();
+    ship.revive();
+    gameOverText.visible = false;
+}
 
-ship = game.add.sprite(400, 300, 'ship');
-ship.scale.set(2);
-ship.smoothed = false;
-ship.animations.add('fly', [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 9, 9, 8, 8, 7, 7, 6, 6, 5, 5, 4, 4], 20, true);
-ship.play('fly');
-
-game.physics.enable(ship, Phaser.Physics.ARCADE);
-game.camera.follow(ship);
-ship.body.collideWorldBounds = true;
-
-bullets = game.add.group();
-bullets.enableBody = true;
-bullets.physicsBodyType = Phaser.Physics.ARCADE;
-
-bullets.createMultiple(50, 'bullet');
-bullets.setAll('checkWorldBounds', true);
-bullets.setAll('outOfBoundsKill', true);
-
-monsters = game.add.group();
-monsters.enableBody = true;
-monsters.phisicsBodyType = Phaser.Physics.ARCADE;
-
-/*createMonsters();
+function createLives(){
+    for(var i = 0; i < numberOfLives; i++)
+        var heart = hearts.create(250 - (30 * i), 560, 'heart')
+}
 
 function createMonsters(){
     
     var count = 0;
-    var inverseToTolerance = tolerance;
+    var inverseTolerance = tolerance;
     
     while(count < numberOfMonsters){
-        inverseToTolerence *= -1;
-        var randx = game.world.randomX + inverseToTolerance;
-        var randy = game.world.randomY + inverseToTolerance;
+        inverseTolerance *= -1;
+        var randx = game.world.randomX + inverseTolerance;
+        var randy = game.world.randomY + inverseTolerance;
         
         if(ship.position.x - tolerance < randx === randx < ship.position.x + tolerance){
             continue;
@@ -83,8 +139,7 @@ function createMonsters(){
     }
     
 }
-*/
-}
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -97,7 +152,50 @@ function update() {
    
     shipFireControls();
     
-    function fire() {
+    collisionDetection();
+    
+    monsterMovement();
+}
+
+function monsterMovement(){
+    monsters.forEach(
+        function (singleMonster){
+            game.physics.arcade.moveToObject(singleMonster, ship, monsterSpeed);
+        },    
+        game.physics
+        
+    );
+}
+
+function dIE(bullet, monster){
+        
+    bullet.kill();
+    monster.kill();
+        
+    }
+    
+function youDIE(you, monster){
+    
+    monster.kill();
+    
+    var heart = hearts.getFirstAlive();
+    
+    if(heart){
+        heart.kill();
+    }
+    if(hearts.countLiving() < 1){
+        you.kill();
+    }
+}
+    
+function collisionDetection(){
+        
+    game.physics.arcade.overlap(bullets, monsters, dIE, null, this);
+    
+    game.physics.arcade.overlap(ship, monsters, youDIE, null, this);
+}
+    
+function fire() {
     if (game.time.now > nextFire && bullets.countDead() > 0){
         nextFire = game.time.now + fireRate;
         var bullet = bullets.getFirstDead();
@@ -140,7 +238,5 @@ function shipFireControls(){
     if (game.input.activePointer.isDown){
         fire();
     }
-
-}
 
 }
